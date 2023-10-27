d99a4a993867f23ec44b7f5c60658a2f8e6363cd
franxu99 <110120388+franxu99@users.noreply.github.com>
2023-10-27 19:17:08 +0200

    0.13

--- src/data/data.js
+++ src/data/data.js
@@ -1,13 +1,23 @@
+import imgPiscina from '../img/piscina.jpg';
+import imgPatioMedio from '../img/patio-medio1.jpeg';
+import imgSalon from '../img/salon1.jpeg';
+import imgPatioBajo from '../img/patio-bajo1.jpeg';
+import imgPatioAlto from '../img/patio-alto1.jpeg';
+import imgPatioBajo2 from '../img/patio-bajo2.jpeg';
+import imgPatioBajo3 from '../img/patio-bajo3.jpeg';
+import imgPatioBajo4 from '../img/patio-bajo4.jpeg';
+import imgPatioBajo5 from '../img/patio-bajo5.jpeg';
+import imgPatioMedio2 from '../img/patio-medio2.jpeg';
 
 export const images = [
-    '../img/patio-alto1.jpeg',
-    '../img/patio-bajo1.jpeg',
-    '../img/patio-bajo2.jpeg',
-    '../img/patio-bajo3.jpeg',
-    '../img/patio-bajo4.jpeg',
-    '../img/patio-bajo5.jpeg',
-    '../img/patio-medio1.jpeg',
-    '../img/patio-medio2.jpeg',
-    '../img/piscina.jpg',
-    '../img/salon1.jpeg'
-]
+    imgPatioAlto,
+    imgPatioBajo,
+    imgPatioBajo2,
+    imgPatioBajo3,
+    imgPatioBajo4,
+    imgPatioBajo5,
+    imgPatioMedio,
+    imgPatioMedio2,
+    imgPiscina,
+    imgSalon
+];
